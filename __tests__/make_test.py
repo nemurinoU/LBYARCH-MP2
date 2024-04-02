a2036ba2e9ab1f3c137c45d7ef8546a219d92211
nemurinoU <63938840+nemurinoU@users.noreply.github.com>
2024-04-02 12:25:09 +0800

    test: add test maker

---
+++ __tests__/make_test.py
@@ -0,0 +1,31 @@
+import glob
+import random
+import os
+from tqdm import tqdm
+
+def double():
+    return random.uniform(-999.99, 999.99)
+
+filenames = [file for file in glob.glob('input*.txt')]
+
+last_filename = filenames[-1]
+counter = int(last_filename .split('.')[0][-1]) + 1
+
+# random.randint(1, 2**30)
+with open(f"input{counter}.txt", "w") as f:
+    len = int(10e7)
+    f.write(f"{len}\n") # vector length
+    f.write(f"{2.0}\n") # scalar variable
+    
+    # vector X
+    for i in tqdm(range(len)):
+        f.write(f"{double()} ")
+    f.write(f"\n")
+
+    # vector Y
+    for i in tqdm(range(len)):
+        f.write(f"{double()} ")
+    f.write(f"\n")
+    pass
+
+
